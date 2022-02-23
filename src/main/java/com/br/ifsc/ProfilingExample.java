17dc9809015a0e66a5acdec2bf878e9608849d6a
Acacio.coding <acacio.coding@gmail.com>
2022-02-22 22:19:55 -0300

    Fix for Krawczyk algorithm during profiling

--- src/main/java/com/br/ifsc/ProfilingExample.java
+++ src/main/java/com/br/ifsc/ProfilingExample.java
@@ -12,15 +12,28 @@ import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class ProfilingExample {
     public static void main(String[] args) throws IOException, WeakSecurityException, NoSuchAlgorithmException, InvalidVSSScheme, NotSplittedException, ReconstructionException {
+        Scanner scanner = new Scanner(System.in);
+        int op = 0;
+
         FileWriter writer = new FileWriter("timings2.txt");
         Map<String, String> data = new HashMap<>();
 
         data.put("1",  RandomStringUtils.random(90000000, true, true));
 
-        getTimings(data, writer, new EngineMaker(10, 5));
+
+        while(op != 1) {
+            System.out.println("\n1 - Sair");
+            System.out.println("2 - Share secret");
+            System.out.println("Option:");
+            op = scanner.nextInt();
+
+            if (op == 2)
+                getTimings(data, writer, new EngineMaker(10, 5));
+        }
 
         writer.close();
     }
@@ -29,66 +42,66 @@ public class ProfilingExample {
         long start;
         long end;
 
-        //Shamir ------------------------------------------------------------------------
-        System.out.println("\n------------ Shamir");
-        writer.write("Shamir algorithm:");
-
-        //Split
-        start = System.nanoTime();
-        engineMaker.split(data, "shamir");
-        end = System.nanoTime() - start;
-
-        writer.write("\n- Share generation time: " + end / 1e+6 + "ms");
-
-        //Reconstruction
-        System.out.println("\nShare reconstructed:");
-        start = System.nanoTime();
-        engineMaker.reconstruct("shamir");
-        end = System.nanoTime() - start;
-
-        writer.write("\n- Share reconstruction time: " + end / 1e+6 + "ms\n\n");
-
-
-        //PSS ---------------------------------------------------------------------------
-        System.out.println("\n------------ PSS");
-        writer.write("PSS algorithm:");
-
-        //Split
-        start = System.nanoTime();
-        engineMaker.split(data, "pss");
-        end = System.nanoTime() - start;
-
-        writer.write("\n- Share generation time: " + end / 1e+6 + "ms");
-
-        //Reconstruction
-        System.out.println("\nShare reconstructed:");
-        start = System.nanoTime();
-        engineMaker.reconstruct("pss");
-        end = System.nanoTime() - start;
-
-        writer.write("\n- Share reconstruction time: " + end / 1e+6 + "ms\n\n");
+//        //Shamir ------------------------------------------------------------------------
+//        System.out.println("\n------------ Shamir");
+//        writer.write("Shamir algorithm:");
+//
+//        //Split
+//        start = System.nanoTime();
+//        engineMaker.split(data, "shamir");
+//        end = System.nanoTime() - start;
+//
+//        writer.write("\n- Share generation time: " + end / 1e+6 + "ms");
+//
+//        //Reconstruction
+//        start = System.nanoTime();
+//        engineMaker.reconstruct("shamir");
+//        end = System.nanoTime() - start;
+//
+//        writer.write("\n- Share reconstruction time: " + end / 1e+6 + "ms\n\n");
+//        System.out.println("Done!");
 
 
-        //CSS ---------------------------------------------------------------------------
-        System.out.println("\n------------ CSS");
-        writer.write("CSS algorithm:");
+//        //PSS ---------------------------------------------------------------------------
+//        System.out.println("\n------------ PSS");
+//        writer.write("PSS algorithm:");
+//
+//        //Split
+//        start = System.nanoTime();
+//        engineMaker.split(data, "pss");
+//        end = System.nanoTime() - start;
+//
+//        writer.write("\n- Share generation time: " + end / 1e+6 + "ms");
+//
+//        //Reconstruction
+//        start = System.nanoTime();
+//        engineMaker.reconstruct("pss");
+//        end = System.nanoTime() - start;
+//
+//        writer.write("\n- Share reconstruction time: " + end / 1e+6 + "ms\n\n");
+//        System.out.println("Done!");
 
-        //Split
-        start = System.nanoTime();
-        engineMaker.split(data, "css");
-        end = System.nanoTime() - start;
 
-        writer.write("\n- Share generation time: " + end / 1e+6 + "ms");
+//        //CSS ---------------------------------------------------------------------------
+//        System.out.println("\n------------ CSS");
+//        writer.write("CSS algorithm:");
+//
+//        //Split
+//        start = System.nanoTime();
+//        engineMaker.split(data, "css");
+//        end = System.nanoTime() - start;
+//
+//        writer.write("\n- Share generation time: " + end / 1e+6 + "ms");
+//
+//        //Reconstruction
+//        start = System.nanoTime();
+//        engineMaker.reconstruct("css");
+//        end = System.nanoTime() - start;
+//
+//        writer.write("\n- Share reconstruction time: " + end / 1e+6 + "ms\n\n");
+//        System.out.println("Done!");
 
-        //Reconstruction
-        System.out.println("\nShare reconstructed:");
-        start = System.nanoTime();
-        engineMaker.reconstruct("css");
-        end = System.nanoTime() - start;
-
-        writer.write("\n- Share reconstruction time: " + end / 1e+6 + "ms\n\n");
-
-
+//
         //Krawczyk ----------------------------------------------------------------------
         System.out.println("\n------------ Krawczyk");
         writer.write("Krawczyk algorithm:");
@@ -101,31 +114,31 @@ public class ProfilingExample {
         writer.write("\n- Share generation time: " + end / 1e+6 + "ms");
 
         //Reconstruction
-        System.out.println("\nShare reconstructed:");
         start = System.nanoTime();
         engineMaker.reconstruct("krawczyk");
         end = System.nanoTime() - start;
 
         writer.write("\n- Share reconstruction time: " + end / 1e+6 + "ms\n\n");
+        System.out.println("Done!");
 
-
-        //PVSS --------------------------------------------------------------------------
-        System.out.println("\n------------ PVSS");
-        writer.write("PVSS algorithm:");
-
-        //Split
-        start = System.nanoTime();
-        engineMaker.split(data, "pvss");
-        end = System.nanoTime() - start;
-
-        writer.write("\n- Share generation time: " + end / 1e+6 + "ms");
-
-        //Reconstruction
-        System.out.println("\nShare reconstructed:");
-        start = System.nanoTime();
-        engineMaker.reconstruct("pvss");
-        end = System.nanoTime() - start;
-
-        writer.write("\n- Share reconstruction time: " + end / 1e+6 + "ms\n\n");
+//
+//        //PVSS --------------------------------------------------------------------------
+//        System.out.println("\n------------ PVSS");
+//        writer.write("PVSS algorithm:");
+//
+//        //Split
+//        start = System.nanoTime();
+//        engineMaker.split(data, "pvss");
+//        end = System.nanoTime() - start;
+//
+//        writer.write("\n- Share generation time: " + end / 1e+6 + "ms");
+//
+//        //Reconstruction
+//        start = System.nanoTime();
+//        engineMaker.reconstruct("pvss");
+//        end = System.nanoTime() - start;
+//
+//        writer.write("\n- Share reconstruction time: " + end / 1e+6 + "ms\n\n");
+//        System.out.println("Done!");
     }
 }
