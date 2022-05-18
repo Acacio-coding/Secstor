9ada373e49e95cadfdcd11f281fd3d951cfdc286
Acacio.coding <acacio.coding@gmail.com>
2022-05-18 12:24:24 -0300

    Auth removal for testing purposes

--- src/main/java/com/ifsc/secstor/api/SecstorApplication.java
+++ src/main/java/com/ifsc/secstor/api/SecstorApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableConfigurationProperties(SecstorConfig.class)
 @EntityScan(basePackages = "com.ifsc.secstor.api.model")
@@ -16,10 +13,5 @@ public class SecstorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecstorApplication.class, args);
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 }
--- src/main/java/com/ifsc/secstor/api/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.ifsc.secstor.api.model;
-
-
-public enum Role {
-    CLIENT, ADMINISTRATOR
-}
